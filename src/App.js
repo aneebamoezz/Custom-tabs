98d1bd609434bf25317cadc4d9071d4bf54420fb
aneebamoezz <anibakaleem@gmail.com>
2024-02-23 14:50:41 +0500

    commit initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TabTest from './components/TabTest';
+
+function App() {
+  return (
+    <div className="App">
+      <TabTest />
+    </div>
+  );
+}
+
+export default App;
